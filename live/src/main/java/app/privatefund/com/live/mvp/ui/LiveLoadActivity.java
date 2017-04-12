e9baa810a6ae807febef480fde06e54905fdfb8a
wangyongkui <beist@sohu.com>
2017-04-12 15:05:11 +0800

    framwork-4.12.1-wyk

---
+++ live/src/main/java/app/privatefund/com/live/mvp/ui/LiveLoadActivity.java
@@ -0,0 +1,16 @@
+package app.privatefund.com.live.mvp.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import app.privatefund.com.live.R;
+
+
+public class LiveLoadActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liveload);
+    }
+}
