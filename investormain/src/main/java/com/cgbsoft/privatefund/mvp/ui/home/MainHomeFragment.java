50e8b821abfcf837dc5cd10ac701a89536972508
wangyongkui <beist@sohu.com>
2017-06-26 21:10:04 +0800

    6.26  首页页面

---
+++ investormain/src/main/java/com/cgbsoft/privatefund/mvp/ui/home/MainHomeFragment.java
@@ -0,0 +1,31 @@
+package com.cgbsoft.privatefund.mvp.ui.home;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.cgbsoft.lib.base.mvp.ui.BaseFragment;
+import com.cgbsoft.privatefund.mvp.contract.home.MainHomeContract;
+import com.cgbsoft.privatefund.mvp.presenter.home.MainHomePresenter;
+
+/**
+ * desc  ${DESC}
+ * author wangyongkui  wangyongkui@simuyun.com
+ * 日期 2017/6/26-21:06
+ */
+public class MainHomeFragment extends BaseFragment<MainHomePresenter> implements MainHomeContract.View {
+
+    @Override
+    protected int layoutID() {
+        return 0;
+    }
+
+    @Override
+    protected void init(View view, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected MainHomePresenter createPresenter() {
+        return null;
+    }
+}
