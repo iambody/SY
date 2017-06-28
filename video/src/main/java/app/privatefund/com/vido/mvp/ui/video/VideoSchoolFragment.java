765f7f50a1b2453e371b9968559561f716394352
wangyongkui <beist@sohu.com>
2017-06-28 17:42:10 +0800

    6.28   video  首页 登录

--- video/src/main/java/app/privatefund/com/vido/mvp/ui/video/VideoSchoolFragment.java
+++ video/src/main/java/app/privatefund/com/vido/mvp/ui/video/VideoSchoolFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
