da50be898b0c600185a5bfbb01f916ed9270852c
wangyongkui <beist@sohu.com>
2017-07-20 11:30:52 +0800

    7.20   web底部交互    分享处理

--- lib/src/main/java/com/cgbsoft/lib/listener/listener/GestureManager.java
+++ lib/src/main/java/com/cgbsoft/lib/listener/listener/GestureManager.java
@@ -1,80 +1,81 @@
-package com.cgbsoft.lib.listener.listener;
-
-import android.content.Context;
-
-import com.cgbsoft.lib.AppInfStore;
-import com.cgbsoft.lib.AppManager;
-import com.cgbsoft.lib.base.model.UserInfoDataEntity;
-import com.cgbsoft.lib.contant.RouteConfig;
-import com.cgbsoft.lib.utils.constant.RxConstant;
-import com.cgbsoft.lib.utils.rxjava.RxBus;
-import com.cgbsoft.lib.utils.tools.NavigationUtils;
-import com.cgbsoft.lib.widget.dialog.DefaultDialog;
-
-/**
- * @author chenlong
- *
- * 手势密码管理类
- */
-public class GestureManager {
-    public static final String HAD_GESTRUE_PASSWORD = "1";
-    public static void showAssertGestureManager(Context context) {
-        UserInfoDataEntity.UserInfo userInfo = AppManager.getUserInfo(context);
-        if (HAD_GESTRUE_PASSWORD.equals(userInfo.getToC().getGestureSwitch())) {
-            if (isLargeTime(context)) {
-                NavigationUtils.startActivityByRouter(context, RouteConfig.VALIDATE_GESTURE_PASSWORD, "PARAM_FROM_SHWO_ASSERT", true);
-            } else {
-                RxBus.get().post(RxConstant.SWITCH_ASSERT_SHOW, true);
-            }
-        } else {
-            showSetGestureDialog(context);
-        }
-    }
-
-    private static void showSetGestureDialog(Context context) {
-        new DefaultDialog(this, context.getString(R.string.gesture_new_no_dialog_desc), context.getString(R.string.gesture_new_no_set), context.getString(R.string.button_ok)) {
-            @Override
-            public void left() {
-                this.dismiss();
-                RxBus.get().post(RxConstant.SWITCH_ASSERT_SHOW, true);
-            }
-
-            @Override
-            public void right() {
-                this.dismiss();
-                NavigationUtils.startActivityByRouter(context, RouteConfig.SET_GESTURE_PASSWORD, "PARAM_FROM_SHWO_ASSERT", true);
-            }
-        }.show();
-    }
-
-    /**
-     * 手势页面拦截器
-     * @param context
-     * @param userInfo
-     * @return
-     */
-    public static boolean intercepterGestureActivity(Context context, UserInfoDataEntity.UserInfo userInfo, boolean resetLogin) {
-        if (userInfo == null) {
-            return false;
-        }
-        boolean isLargeTime = isLargeTime(context);
-        if ((HAD_GESTRUE_PASSWORD.equals(userInfo.getToC().getGestureSwitch())) && (resetLogin || isLargeTime)) {
-            NavigationUtils.startActivityByRouter(context, RouteConfig.VALIDATE_GESTURE_PASSWORD, "PARAM_FROM_LOGIN", true);
-            return true;
-        } else {
-            if (!HAD_GESTRUE_PASSWORD.equals(userInfo.getToC().getGestureSwitch()) && AppManager.getUserFirstLogin(context)) {
-                NavigationUtils.startActivityByRouter(context, RouteConfig.SET_GESTURE_PASSWORD, "PARAM_FROM_REGEIST_OR_LOGIN", true);
-                AppInfStore.saveUserFirstLogin(context, false);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean isLargeTime(Context context) {
-        long currentTime = System.currentTimeMillis();
-        long preTime = AppManager.getLastSetOrValidateTime(context);
-        System.out.println("-------time=" + (currentTime - preTime));
-        return (currentTime - preTime)/1000 > 120;
-    }
-}
+//package com.cgbsoft.lib.listener.listener;
+//
+//import android.content.Context;
+//
+//import com.cgbsoft.lib.AppInfStore;
+//import com.cgbsoft.lib.AppManager;
+//import com.cgbsoft.lib.base.model.UserInfoDataEntity;
+//import com.cgbsoft.lib.contant.RouteConfig;
+//import com.cgbsoft.lib.utils.constant.RxConstant;
+//import com.cgbsoft.lib.utils.rxjava.RxBus;
+//import com.cgbsoft.lib.utils.tools.NavigationUtils;
+//import com.cgbsoft.lib.widget.dialog.DefaultDialog;
+//import com.cgbsoft.lib.R;
+//
+///**
+// * @author chenlong
+// *
+// * 手势密码管理类
+// */
+//public class GestureManager {
+//    public static final String HAD_GESTRUE_PASSWORD = "1";
+//    public static void showAssertGestureManager(Context context) {
+//        UserInfoDataEntity.UserInfo userInfo = AppManager.getUserInfo(context);
+//        if (HAD_GESTRUE_PASSWORD.equals(userInfo.getToC().getGestureSwitch())) {
+//            if (isLargeTime(context)) {
+//                NavigationUtils.startActivityByRouter(context, RouteConfig.VALIDATE_GESTURE_PASSWORD, "PARAM_FROM_SHWO_ASSERT", true);
+//            } else {
+//                RxBus.get().post(RxConstant.SWITCH_ASSERT_SHOW, true);
+//            }
+//        } else {
+//            showSetGestureDialog(context);
+//        }
+//    }
+//
+//    private static void showSetGestureDialog(Context context) {
+//        new DefaultDialog(this, context.getString(R.string.gesture_new_no_dialog_desc), context.getString(R.string.gesture_new_no_set), context.getString(R.string.button_ok)) {
+//            @Override
+//            public void left() {
+//                this.dismiss();
+//                RxBus.get().post(RxConstant.SWITCH_ASSERT_SHOW, true);
+//            }
+//
+//            @Override
+//            public void right() {
+//                this.dismiss();
+//                NavigationUtils.startActivityByRouter(context, RouteConfig.SET_GESTURE_PASSWORD, "PARAM_FROM_SHWO_ASSERT", true);
+//            }
+//        }.show();
+//    }
+//
+//    /**
+//     * 手势页面拦截器
+//     * @param context
+//     * @param userInfo
+//     * @return
+//     */
+//    public static boolean intercepterGestureActivity(Context context, UserInfoDataEntity.UserInfo userInfo, boolean resetLogin) {
+//        if (userInfo == null) {
+//            return false;
+//        }
+//        boolean isLargeTime = isLargeTime(context);
+//        if ((HAD_GESTRUE_PASSWORD.equals(userInfo.getToC().getGestureSwitch())) && (resetLogin || isLargeTime)) {
+//            NavigationUtils.startActivityByRouter(context, RouteConfig.VALIDATE_GESTURE_PASSWORD, "PARAM_FROM_LOGIN", true);
+//            return true;
+//        } else {
+//            if (!HAD_GESTRUE_PASSWORD.equals(userInfo.getToC().getGestureSwitch()) && AppManager.getUserFirstLogin(context)) {
+//                NavigationUtils.startActivityByRouter(context, RouteConfig.SET_GESTURE_PASSWORD, "PARAM_FROM_REGEIST_OR_LOGIN", true);
+//                AppInfStore.saveUserFirstLogin(context, false);
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    private static boolean isLargeTime(Context context) {
+//        long currentTime = System.currentTimeMillis();
+//        long preTime = AppManager.getLastSetOrValidateTime(context);
+//        System.out.println("-------time=" + (currentTime - preTime));
+//        return (currentTime - preTime)/1000 > 120;
+//    }
+//}
