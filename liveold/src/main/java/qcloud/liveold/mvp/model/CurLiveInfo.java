8166f73a9182fdb3cbc271081c7d89abbc120d71
yangzonghui <yangzonghui@simuyun.com>
2017-07-30 20:41:36 +0800

    bug fix

--- liveold/src/main/java/qcloud/liveold/mvp/model/CurLiveInfo.java
+++ liveold/src/main/java/qcloud/liveold/mvp/model/CurLiveInfo.java
@@ -77,7 +77,7 @@ public class CurLiveInfo {
 
     public static String getShareUrl() {
         try {
-            return String.format("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb19b10cf492481cf&redirect_uri=%s&response_type=code&scope=snsapi_login&state=00000#wechat_redirect",
+            return String.format("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx1622f3ec2d611b59&redirect_uri=%s&response_type=code&scope=snsapi_login&state=00000#wechat_redirect",
                     URLEncoder.encode(
                             String.format(
                                     "%s?userId=%s&roomId=%d",
