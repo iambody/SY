046f25d4378c3973deb72e96bb496c3ad9e7b700
chenlong <chenlong@simuyun.com>
2017-07-31 21:31:21 +0800

    bug fix

--- health/src/main/java/app/privatefund/investor/health/mvp/ui/CheckHealthFragment.java
+++ health/src/main/java/app/privatefund/investor/health/mvp/ui/CheckHealthFragment.java
@@ -3,7 +3,6 @@ package app.privatefund.investor.health.mvp.ui;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 
