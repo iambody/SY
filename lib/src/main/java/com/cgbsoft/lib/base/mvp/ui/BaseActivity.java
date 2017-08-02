7e9e1411941f8668f0d76e4520231188df1f9d6e
yangzonghui <yangzonghui@simuyun.com>
2017-08-02 18:48:47 +0800

    bug fix

--- lib/src/main/java/com/cgbsoft/lib/base/mvp/ui/BaseActivity.java
+++ lib/src/main/java/com/cgbsoft/lib/base/mvp/ui/BaseActivity.java
@@ -88,6 +88,7 @@ public abstract class BaseActivity<P extends BasePresenterImpl> extends RxAppCom
             init(savedInstanceState);
             data();
         }
+        AppInfStore.saveDialogTag(BaseActivity.this,false);
         registerLogoutBroadcast();
     }
 
