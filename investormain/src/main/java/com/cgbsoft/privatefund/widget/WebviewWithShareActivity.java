473134c82f17dcc31648895e3d168ee48c41091b
chenlong <chenlong@simuyun.com>
2017-08-15 19:19:48 +0800

    delete tocshare instruct

--- investormain/src/main/java/com/cgbsoft/privatefund/widget/WebviewWithShareActivity.java
+++ investormain/src/main/java/com/cgbsoft/privatefund/widget/WebviewWithShareActivity.java
@@ -5,13 +5,13 @@ import com.cgbsoft.lib.base.webview.BaseWebNetConfig;
 import com.cgbsoft.lib.base.webview.BaseWebViewActivity;
 import com.cgbsoft.lib.base.webview.WebViewConstant;
 import com.cgbsoft.lib.contant.RouteConfig;
+import com.cgbsoft.lib.share.bean.ShareCommonBean;
+import com.cgbsoft.lib.share.dialog.CommonShareDialog;
 import com.cgbsoft.lib.utils.tools.LogUtils;
 import com.chenenyu.router.annotation.Route;
 
 import java.net.URLDecoder;
 
-import app.privatefund.com.share.bean.ShareCommonBean;
-import app.privatefund.com.share.dialog.CommonShareDialog;
 
 /**
  * @author chenlong
