542defc7e4da3d213ea532739bfe54245bd073bc
yangzonghui <yangzonghui@simuyun.com>
2017-08-21 17:56:38 +0800

    bug fix

--- lib/src/main/java/com/cgbsoft/lib/base/webview/CWebviewManger.java
+++ lib/src/main/java/com/cgbsoft/lib/base/webview/CWebviewManger.java
@@ -337,9 +337,10 @@ public class CWebviewManger {
 //            PromptManager.ShowCustomToast(context,"我需要分享名片");
             shareAdviserCardShow(action);
         } else if (action.contains("QRCallBack")) {
-            Intent intent = new Intent(context, CaptureActivity.class);
-            intent.putExtra("isCallBack", "Y");
-            context.startActivity(intent);
+            
+//            Intent intent = new Intent(context, CaptureActivity.class);
+//            intent.putExtra("isCallBack", "Y");
+//            context.startActivity(intent);
         } else if (action.contains("sharePoster")) {
 //            sharePoster();
         } else if (action.contains("ydPay")) {
