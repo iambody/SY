9bfd20233d3d24df3bd6f70d4278c68080d91910
wangyongkui <beist@sohu.com>
2017-08-23 10:44:47 +0800

    8.23   定位崩溃问题&& bugfixs

---
+++ lib/src/main/java/com/cgbsoft/lib/utils/Rxbus/RxBusManger.java
@@ -0,0 +1,13 @@
+package com.cgbsoft.lib.utils.Rxbus;
+
+
+import rx.subjects.Subject;
+
+/**
+ * desc  ${DESC}
+ * author wangyongkui  wangyongkui@simuyun.com
+ * 日期 2017/8/22-16:38
+ */
+public class RxBusManger {
+    private final Subject<Object, Object> _bus;
+}
---
+++ video/src/main/java/app/privatefund/com/vido/VideoUtil.kt
@@ -0,0 +1,7 @@
+package app.privatefund.com.vido
+
+/**
+ *desc  ${DESC}
+ *author wangyongkui  wangyongkui@simuyun.com
+ *日期 2017/8/21-18:06
+ */
