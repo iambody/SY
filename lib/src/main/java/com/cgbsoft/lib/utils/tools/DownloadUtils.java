783d9f07af1181350a8fbed2dc8afb749ed8aa1f
chenlong <chenlong@simuyun.com>
2017-09-06 15:54:35 +0800

    moidfy file upload suffix

--- lib/src/main/java/com/cgbsoft/lib/utils/tools/DownloadUtils.java
+++ lib/src/main/java/com/cgbsoft/lib/utils/tools/DownloadUtils.java
@@ -46,7 +46,7 @@ public class DownloadUtils {
         // 表单域
         Map<String, String> textMap = new LinkedHashMap<>();
         String pre = ".png";
-        if (!TextUtils.isEmpty(localFilePath) && localFilePath.contains(".")) {
+        if (!TextUtils.isEmpty(localFilePath) && localFilePath.contains(".") && localFilePath.lastIndexOf(".") < localFilePath.length()) {
             int dex = localFilePath.lastIndexOf(".");
             pre = localFilePath.substring(dex);
         }
