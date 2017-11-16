7a8ada300f60e6344adcbcffb0aab0cd8b705b55
wangyongkui <beist@sohu.com>
2017-11-16 18:03:44 +0800

    不用secret 的图片

--- lib/src/main/java/com/cgbsoft/lib/utils/tools/CameraUtils.java
+++ lib/src/main/java/com/cgbsoft/lib/utils/tools/CameraUtils.java
@@ -1,7 +1,10 @@
 package com.cgbsoft.lib.utils.tools;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Build;
 
 /**
  * desc  ${DESC}
@@ -30,5 +33,32 @@ public class CameraUtils {
             }
             return isCanUse;
         }
+    }
+
+    /**
+     * 跳转到设置页面
+     * @param context
+     */
+    public static void toSetCameraPermission(Context context) {
+        if (getCameraPermission(context)) {
+        } else {
+            PromptManager.ShowCustomToast(context, "请设置允许拍照权限");
+            toSelfSetting(context);
+        }
+    }
+
+
+    public static void toSelfSetting(Context context) {
+        Intent mIntent = new Intent();
+        mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (Build.VERSION.SDK_INT >= 9) {
+            mIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+            mIntent.setData(Uri.fromParts("package", context.getPackageName(), null));
+        } else if (Build.VERSION.SDK_INT <= 8) {
+            mIntent.setAction(Intent.ACTION_VIEW);
+            mIntent.setClassName("com.android.settings", "com.android.setting.InstalledAppDetails");
+            mIntent.putExtra("com.android.settings.ApplicationPkgName", context.getPackageName());
+        }
+        context.startActivity(mIntent);
     }
 }
