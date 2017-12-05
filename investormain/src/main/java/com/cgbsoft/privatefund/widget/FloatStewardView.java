58c8b764d132a921c5c7b87418ecfb4b7be3ad79
wangyongkui <beist@sohu.com>
2017-12-05 21:26:53 +0800

    首页float悬浮

--- investormain/src/main/java/com/cgbsoft/privatefund/widget/FloatStewardView.java
+++ investormain/src/main/java/com/cgbsoft/privatefund/widget/FloatStewardView.java
@@ -1,13 +1,18 @@
 package com.cgbsoft.privatefund.widget;
 
 import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.cgbsoft.lib.utils.tools.DimensionPixelUtil;
+import com.cgbsoft.lib.utils.tools.Utils;
 import com.cgbsoft.lib.widget.RoundImageView;
 import com.cgbsoft.privatefund.R;
 
@@ -18,13 +23,14 @@ import com.cgbsoft.privatefund.R;
  * 日期 2017/11/29-15:06
  */
 public class FloatStewardView extends RelativeLayout implements View.OnClickListener {
-    private boolean isVisitor;
+    private boolean isVisitor = false;
+    private Context floatContext;
     private SemicircleView semicircleview;
 
     private RoundImageView steward_round_iv;
     //外层轮廓,外层轮廓
-    private LinearLayout rectangle_out_lay, rectangle_in_lay;
-
+    private LinearLayout rectangle_in_lay;
+    private RelativeLayout rectangle_out_lay;
     private View baseView;
 
     private AnimatorSet openVisitorAnmatorSet, openUserAnmatorSet, closeVisitorAnmatorSet, closeUserAnmatorSet;
@@ -49,11 +55,13 @@ public class FloatStewardView extends RelativeLayout implements View.OnClickList
      * 开始初始化view
      */
     private void initView(Context context) {
+        this.floatContext = context;
         baseView = View.inflate(context, R.layout.view_float_steward, this);
 
         semicircleview = (SemicircleView) baseView.findViewById(R.id.semicircleview);
         steward_round_iv = (RoundImageView) baseView.findViewById(R.id.steward_round_iv);
-        rectangle_out_lay = (LinearLayout) baseView.findViewById(R.id.rectangle_out_lay);
+        rectangle_out_lay = (RelativeLayout) baseView.findViewById(R.id.rectangle_out_lay);
+        rectangle_in_lay = (LinearLayout) baseView.findViewById(R.id.rectangle_in_lay);
         steward_round_iv.setOnClickListener(this);
     }
 
@@ -64,13 +72,17 @@ public class FloatStewardView extends RelativeLayout implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.steward_round_iv:
+                openFloat();
+                break;
+        }
     }
 
     /**
      * 初始化
      **/
-    public void initVisitor(boolean isvisitor, FloatStewardListener stewardListener) {
+    public void initFloat(boolean isvisitor, FloatStewardListener stewardListener) {
         this.isVisitor = isvisitor;
         this.floatStewardListener = stewardListener;
     }
@@ -78,17 +90,63 @@ public class FloatStewardView extends RelativeLayout implements View.OnClickList
     /**
      * 张开
      **/
-    public void openVisitor() {
+    public void openFloat() {
+        int surplusWidth = Utils.getScreenWidth(floatContext) - DimensionPixelUtil.dip2px(floatContext, 160);
+        int surplusWidth1 = Utils.getScreenWidth(floatContext) - DimensionPixelUtil.dip2px(floatContext, 120);
         if (isVisitor) {
-//            ObjectAnimator objectAnimator=ObjectAnimator.ofFloat(rectangle_out_lay,"scaleX",1f,3f)
+            //游客
+            ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 100);
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+
+                    int animatorValue = (int) animation.getAnimatedValue();
+                    Log.i("sskkk", "位置" + animatorValue);
+                    float fraction = animatorValue / 100f;
+                    rectangle_in_lay.getLayoutParams().width =  DimensionPixelUtil.dip2px(floatContext, 40) + (int) (surplusWidth * fraction);
+                    rectangle_in_lay.requestLayout();
+                }
+            });
+            valueAnimator.setInterpolator(new BounceInterpolator());//LinearInterpolator
+            valueAnimator.setDuration(3 * 1000);
+            valueAnimator.setTarget(rectangle_in_lay);
+            valueAnimator.start();
         } else {
+            //用户
+
+            ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 100);
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+
+                    int animatorValue = (int) animation.getAnimatedValue();
+                    Log.i("sskkk", "位置" + animatorValue);
+                    float fraction = animatorValue / 100f;
+                    rectangle_in_lay.getLayoutParams().width =  DimensionPixelUtil.dip2px(floatContext, 40) + (int) (surplusWidth * fraction);
+
+                    rectangle_in_lay.requestLayout();
+
+                    rectangle_out_lay.getLayoutParams().height= DimensionPixelUtil.dip2px(floatContext, 80)+(int)(fraction* DimensionPixelUtil.dip2px(floatContext, 20));
+                    rectangle_out_lay.requestLayout();
+
+                    semicircleview.getLayoutParams().height=rectangle_out_lay.getLayoutParams().height;
+                    semicircleview.getLayoutParams().width=rectangle_out_lay.getLayoutParams().height;
+                    semicircleview.requestLayout();
+
+                }
+            });
+            valueAnimator.setInterpolator(new BounceInterpolator());//LinearInterpolator
+            valueAnimator.setDuration(3 * 1000);
+//            valueAnimator.setTarget(rectangle_in_lay,rectangle_out_lay);
+            valueAnimator.start();
+
         }
     }
 
     /**
      * 关闭
      **/
-    public void closeVisitor() {
+    public void closeFloat() {
     }
 
 
