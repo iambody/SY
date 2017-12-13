c39f8979c22d3383174acb7c48cc9bc3d3b1e6ee
wangyongkui <beist@sohu.com>
2017-12-13 10:07:44 +0800

    app启动优化

--- investormain/src/main/java/com/cgbsoft/privatefund/utils/service/InitializeService.java
+++ investormain/src/main/java/com/cgbsoft/privatefund/utils/service/InitializeService.java
@@ -45,34 +45,38 @@ import qcloud.liveold.mvp.utils.SxbLogImpl;
  * 日期 2017/12/12-15:14
  */
 public class InitializeService extends IntentService {
+    private final String TAG = "InitializeService";
     public static final String ACTION_INIT_WHEN_APP_CREATE = "com.cgbsoft.privatefund.service.action.app.create";
     public static final String EXTRA_PARAM = "com.cgbsoft.privatefund.service.extra.PARAM";
+
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
      * @param name Used to name the worker thread, important only for debugging.
      */
     public InitializeService() {
         super("InitializeService");
     }
+
     public static void startService(Context context) {
         Intent intent = new Intent(context, InitializeService.class);
         intent.setAction(ACTION_INIT_WHEN_APP_CREATE);
         context.startService(intent);
-        Log.i("okgogoogog","开始startService");
     }
+
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        Log.i("okgogoogog","开始onHandleIntent");
+        Log.i(TAG, "开始onHandleIntent");
         if (null != intent) {
             String action = intent.getAction();
             if (ACTION_INIT_WHEN_APP_CREATE.equals(action)) {
-
                 performInit(action);
             }
         }
     }
+
     private void performInit(String action) {
-        Log.i("okgogoogog","开始performInit");
+        Log.i(TAG, "开始performInit");
         initOkGo();//初始化okgo
         initImageLoader();//初始化图片加载
         initLive();//初始化直播
@@ -84,6 +88,7 @@ public class InitializeService extends IntentService {
         initLearCanary();//初始化内检测
 
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -98,37 +103,20 @@ public class InitializeService extends IntentService {
 
     private void initX5() {
         //x5内核初始化接口
-        ((InitApplication)InitializeService.this. getApplication()).initX5Webview();
-//        try {
-//            QbSdk.initX5Environment(InitializeService.this.getApplicationContext(), new QbSdk.PreInitCallback() {
-//                @Override
-//                public void onCoreInitFinished() {
-//
-//                }
-//
-//                @Override
-//                public void onViewInitFinished(boolean b) {
-//
-//                }
-//            });
-//        } catch (Exception e) {
-//            LogUtils.Log("BaseApplication", e.getMessage());
-//            e.printStackTrace();
-//        }
+        ((InitApplication) InitializeService.this.getApplication()).initX5Webview();
     }
 
     private void initIm() {
         /**
          * OnCreate 会被多个进程重入，这段保护代码，确保只有您需要使用 RongIM 的进程和 Push 进程执行了 init。
-         * io.rong.push 为融云 push 进程名称，不可修改。
+         * io.rong.push 为融云 push 进程名称，不可修改。 // "io.rong.push".equals(DeviceUtils.getCurProcessName(getApplicationContext()))
          */
-        // "io.rong.push".equals(DeviceUtils.getCurProcessName(getApplicationContext()))
-        if ( InitializeService.this.getApplication() .getApplicationInfo().packageName.equals(DeviceUtils.getCurProcessName( InitializeService.this.getApplicationContext()))) {
-            Log.i("InitApplication", "----initRongConnect");
+
+        if (InitializeService.this.getApplication().getApplicationInfo().packageName.equals(DeviceUtils.getCurProcessName(InitializeService.this.getApplicationContext()))) {
             /**
              * IMKit SDK调用第一步 初始化
              */
-            if ( InitializeService.this.getApplication() .getApplicationInfo().packageName.equals(DeviceUtils.getCurProcessName( InitializeService.this.getApplicationContext()))) {
+            if (InitializeService.this.getApplication().getApplicationInfo().packageName.equals(DeviceUtils.getCurProcessName(InitializeService.this.getApplicationContext()))) {
                 RongIM.init(((InitApplication) InitializeService.this.getApplication()));
             }
             RongIM.registerMessageType(ProductMessage.class);
@@ -152,37 +140,24 @@ public class InitializeService extends IntentService {
     }
 
     private void initLive() {
-//        SxbLogImpl.init(InitializeService.this.getApplicationContext());
+        // SxbLogImpl.init(InitializeService.this.getApplicationContext());
         //初始化APP
         InitBusinessHelper.initApp(InitializeService.this.getApplicationContext());
     }
 
     private void initOkGo() {
-        Log.i("okgogoogog","开始service调用okgo");
-        ((InitApplication)InitializeService.this. getApplication()).initOkGo();
-//        OkGo.init((BaseApplication) getApplication());
-//        try {
-//            OkGo.getInstance().debug("OKGO", Level.ALL, true)
-//                    .setConnectTimeout(OKHTTP.HTTP_CONNECTION_TIMEOUT)  //全局的连接超时时间
-//                    .setReadTimeOut(OKHTTP.HTTP_CONNECTION_TIMEOUT)     //全局的读取超时时间
-//                    .setWriteTimeOut(OKHTTP.HTTP_CONNECTION_TIMEOUT)    //全局的写入超时时间
-//                    .setRetryCount(3);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
+        Log.i(TAG, "开始service调用okgo");
+        ((InitApplication) InitializeService.this.getApplication()).initOkGo();
     }
 
     private void initMob() {
         MobclickAgent.openActivityDurationTrack(false);//禁止默认的页面统计方式，这样将不会再自动统计Activity
         MobclickAgent.setDebugMode(true);
-        Thread.setDefaultUncaughtExceptionHandler(new SimuyunUncaughtExceptionHandler( InitializeService.this.getApplication()));
+        Thread.setDefaultUncaughtExceptionHandler(new SimuyunUncaughtExceptionHandler(InitializeService.this.getApplication()));
         MSDKDnsResolver.getInstance().init(InitializeService.this.getApplicationContext());
-
     }
 
     private void initLearCanary() {
-
-        ((InitApplication)InitializeService.this. getApplication()).initLearCanary();
+        ((InitApplication) InitializeService.this.getApplication()).initLearCanary();
     }
 }
