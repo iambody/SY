f749e27b9b5828a902afb1b0acc90c91fa2a1eb9
yangzonghui <yangzonghui@simuyun.com>
2018-02-05 16:12:50 +0800

    添加新年图标

--- investormain/src/main/java/com/cgbsoft/privatefund/adapter/OperationAdapter.java
+++ investormain/src/main/java/com/cgbsoft/privatefund/adapter/OperationAdapter.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.cgbsoft.lib.BaseApplication;
 import com.cgbsoft.lib.base.model.HomeEntity;
 import com.cgbsoft.lib.utils.imgNetLoad.Imageload;
 import com.cgbsoft.lib.utils.tools.BStrUtils;
@@ -39,9 +40,9 @@ public class OperationAdapter extends BaseAdapter {
         this.layoutInflater = LayoutInflater.from(context);
     }
 
-    public void refreshData(List<HomeEntity.Operate> resource,int numcolumns) {
+    public void refreshData(List<HomeEntity.Operate> resource, int numcolumns) {
         this.resourceData = resource;
-        this.numColumns=numcolumns;
+        this.numColumns = numcolumns;
         OperationAdapter.this.notifyDataSetChanged();
     }
 
@@ -72,10 +73,9 @@ public class OperationAdapter extends BaseAdapter {
         } else {
             operationItem = (OperationItem) convertView.getTag();
         }
-        BStrUtils.SetTxt( operationItem.operationdes,resourceData.get(position).title);
-        ivOption(operationItem.logoImageView,numColumns);
-        Imageload.display(context,resourceData.get(position).imageUrl,operationItem.logoImageView);
-
+        BStrUtils.SetTxt(operationItem.operationdes, resourceData.get(position).title);
+        ivOption(operationItem.logoImageView, numColumns);
+        Imageload.display(context, resourceData.get(position).imageUrl, operationItem.logoImageView);
         return convertView;
     }
 
@@ -84,11 +84,11 @@ public class OperationAdapter extends BaseAdapter {
         TextView operationdes;
     }
 
-    private void ivOption(ImageView view,int Columns){
-        int width=Utils.getScreenWidth(context);
-        int itemWidth=((width- DimensionPixelUtil.dip2px(context,(Columns+1)*10))/Columns)*2/3;
-        LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(itemWidth,itemWidth);
-        params.gravity= Gravity.CENTER_HORIZONTAL;
+    private void ivOption(ImageView view, int Columns) {
+        int width = Utils.getScreenWidth(context);
+        int itemWidth = ((width - DimensionPixelUtil.dip2px(context, (Columns + 1) * 10)) / Columns) * 2 / 3;
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(itemWidth, itemWidth);
+        params.gravity = Gravity.CENTER_HORIZONTAL;
         view.setLayoutParams(params);
     }
 }
