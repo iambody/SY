9a9b80ac4c6590b3d786294a93e285a6fde114a3
chenlong <chenlong@simuyun.com>
2018-03-07 15:42:03 +0800

    文案修改

--- investormain/src/main/java/com/cgbsoft/privatefund/mvp/ui/center/BindBankCardInfoActivity.java
+++ investormain/src/main/java/com/cgbsoft/privatefund/mvp/ui/center/BindBankCardInfoActivity.java
@@ -103,12 +103,12 @@ public class BindBankCardInfoActivity extends BaseActivity<BindBankCardInfoPrese
         listView.setOnMenuItemClickListener((position, menu, index) -> {
           if (index == 0) {
               if (bindBankCardAdapter.getCount() == 2) {
-                  Toast.makeText(getApplicationContext(), "至少保留一张银行卡！", Toast.LENGTH_SHORT).show();
+                  Toast.makeText(getApplicationContext(), "至少绑定一张银行卡！", Toast.LENGTH_SHORT).show();
                   return false;
               }
               BindBankCardInfoBean bindBankCardInfoBean = (BindBankCardInfoBean)bindBankCardAdapter.getItem(position);
               String simpleBankName = findNameByChannelId(bindBankCardInfoBean.getChannelid());
-              PayPasswordDialog payPasswordDialog = new PayPasswordDialog(BindBankCardInfoActivity.this, "请输入你的交易密码", simpleBankName, hintLastBankCardNumber(bindBankCardInfoBean.getDepositacct()));
+              PayPasswordDialog payPasswordDialog = new PayPasswordDialog(BindBankCardInfoActivity.this, "请输入您的交易密码", simpleBankName, hintLastBankCardNumber(bindBankCardInfoBean.getDepositacct()));
               payPasswordDialog.setmPassWordInputListener(psw -> {
                   deleteIndex = position;
                   payPasswordDialog.dismiss();
