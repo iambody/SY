8751cff72ee43b63dcd700a7d87ff42fdd44c16e
wangpeng <wangpeng_a@simuyun.com>
2018-04-03 19:49:18 +0800

    修改赎回输入框删除问题

--- investormain/src/main/java/com/cgbsoft/privatefund/public_fund/SellPublicFundActivity.java
+++ investormain/src/main/java/com/cgbsoft/privatefund/public_fund/SellPublicFundActivity.java
@@ -233,7 +233,7 @@ public class SellPublicFundActivity extends BaseActivity<SellPUblicFundPresenter
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (TextUtils.isEmpty(s) && s.equals(input.getText())) return;
+                if (TextUtils.isEmpty(s) || s.equals(input.getText())) return;
                 if (curruntBankCard != null && new BigDecimal(s.toString()).subtract(new BigDecimal(curruntBankCard.getAvailBalMode1())).doubleValue() > 0) {
                     if(isFund){
                         MToast.makeText(SellPublicFundActivity.this,"该卡最大可提现"+curruntBankCard.getAvailBalMode1()+"元",Toast.LENGTH_LONG).show();
